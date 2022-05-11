07d1d55b9b751099b88d209f2be879eba4f13167
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-11 10:59:38 +0530

    Update starter.py

--- AttachMents/starter.py
+++ AttachMents/starter.py
@@ -6,17 +6,17 @@ class start():
 
     @bot.on(events.NewMessage(pattern=r"^/start$|^/start@Anime_Gallery_Robot"))
     async def event_handler_start(_, e: event):
-    TheVenomXD = -1001791685690
-    user = e.from_user.id
-        try:
-            await client.get_chat_member(TheVenomXD, user)
-        except UserNotParticipant:
-            return await message.reply_text("You need to join @channel username to use me.", disable_web_page_preview=True)
-                await bot.send_message(
-                    event.chat_id,
-                    start_text,
-                    file='https://tenor.com/view/chika-fujiwara-kaguya-sama-love-is-war-anime-wink-smile-gif-18043249'
-                )
+        TheVenomXD = -1001791685690
+        user = e.from_user.id
+            try:
+                await client.get_chat_member(TheVenomXD, user)
+            except UserNotParticipant:
+                return await message.reply_text("You need to join @channel username to use me.", disable_web_page_preview=True)
+                    await bot.send_message(
+                        event.chat_id,
+                        start_text,
+                        file='https://tenor.com/view/chika-fujiwara-kaguya-sama-love-is-war-anime-wink-smile-gif-18043249'
+                    )
 
     @bot.on(events.NewMessage(pattern=r"^/help$|^"))
     async def event_handler_help(event):
