85e4737c0d0dd14424a83e479b2b8994e86655c0
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-11 11:16:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,5 @@ from telethon import TelegramClient
 api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
 bot_token = os.environ.get('BOT_TOKEN')
-tsession = os.environ.get('TSESSION')
 
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
-Client = TelegramClient('Client', api_id, api_hash).start(tsession=tsession)
