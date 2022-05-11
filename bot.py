53a485071485deade3f4e03d6a89bb1cdb5c2075
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-11 23:15:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,6 +27,7 @@ else:
     session_name = "startup"
     bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
 
+bot.start()
 
 @bot.on(events.NewMessage(incoming=True, chats=Var.FROM_CHANNEL))
 async def _(event): 
