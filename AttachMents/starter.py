6af220d6366c9dd645900580dc49e50fcc91b638
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-11 11:35:36 +0530

    Update starter.py

--- AttachMents/starter.py
+++ AttachMents/starter.py
@@ -5,7 +5,7 @@ from telethon import events
 class start():
 
     @bot.on(events.NewMessage(pattern=r"^/start$|^/start@Anime_Gallery_Robot"))
-    async def event_handler_start(_, e: event):
+    async def event_handler_start(_, e: events):
         TheVenomXD = -1001791685690
         user = e.from_user.id
         try:
@@ -13,7 +13,7 @@ class start():
         except UserNotParticipant:
             return await message.reply_text("You need to join @channel username to use me.", disable_web_page_preview=True)
             await bot.send_message(
-                event.chat_id,
+                events.chat_id,
                 start_text,
                 file='https://tenor.com/view/chika-fujiwara-kaguya-sama-love-is-war-anime-wink-smile-gif-18043249'
             )
