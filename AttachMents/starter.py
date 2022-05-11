9fd4524f15015d2130ba6e51ab33bc6d840ecbd9
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-11 10:52:19 +0530

    Update starter.py

--- AttachMents/starter.py
+++ AttachMents/starter.py
@@ -5,9 +5,11 @@ from telethon import events
 class start():
 
     @bot.on(events.NewMessage(pattern=r"^/start$|^/start@Anime_Gallery_Robot"))
-    async def event_handler_start(event):
+    async def event_handler_start(_, e: event):
+    TheVenomXD = -1001791685690
+    user = e.from_user.id
         try:
-            await client.get_chat_member(channel id, user)
+            await client.get_chat_member(TheVenomXD, user)
         except UserNotParticipant:
             return await message.reply_text("You need to join @channel username to use me.", disable_web_page_preview=True)
                 await bot.send_message(
