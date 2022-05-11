102fe88684219a581dfdaca22a27b31b7de26b07
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-11 14:17:08 +0530

    Add files via upload

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python main.py
+bot: python3 bot.py
---
+++ bot.py
@@ -0,0 +1,62 @@
+
+import logging
+import asyncio
+from telethon import TelegramClient, events, Button
+from decouple import config
+from telethon.tl.functions.users import GetFullUserRequest
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.INFO)
+
+# start the bot
+print("Starting...")
+print("Initializing RyzenApi")
+print("Initializing Repo")
+try:
+    apiid = config("APP_ID", cast=int)
+    apihash = config("API_HASH")
+    session = config("SESSION")
+    frm = config("FROM_CHANNEL", cast=int)
+    tochnl = config("TO_CHANNEL", cast=int)
+    datgbot = TelegramClient(StringSession(SESSION), API_ID, API_HASH)
+except:
+    print("Environment vars are missing! Kindly recheck.")
+    print("Bot is quiting...")
+    exit()
+    
+
+@datgbot.on(events.NewMessage(pattern="/start"))
+async def _(event):
+    ok = await datgbot(GetFullUserRequest(event.sender_id))
+    await event.reply(f"Hi `{ok.user.first_name}`!\n\nI am a channel auto-post bot!! Read /help to know more!\n\nI can be used in only two channels (one user) at a time. Kindly deploy your own bot.")
+
+
+@datgbot.on(events.NewMessage(pattern="/help"))
+async def helpp(event):
+    await event.reply("Set All Vars And Bot Started Nothing More ;)")
+
+@datgbot.on(events.NewMessage(incoming=True, chats=frm)) 
+async def _(event): 
+    if not event.is_private:
+        try:
+            if event.poll:
+                return
+            if event.photo:
+                photo = event.media.photo
+                await datgbot.send_file(tochnl, photo, caption = event.text, link_preview = False)
+            elif event.media:
+                try:
+                    if event.media.webpage:
+                        await datgbot.send_message(tochnl, event.text, link_preview = False)
+                        return
+                except:
+                    media = event.media.document
+                    await datgbot.send_file(tochnl, media, caption = event.text, link_preview = False)
+                    return
+            else:
+                await datgbot.send_message(tochnl, event.text, link_preview = False)
+        except:
+            print("TO_CHANNEL ID is wrong or I can't send messages there (make me admin).")
+
+
+print("Bot Initialized Perhaps Started!")
+datgbot.run_until_disconnected()
