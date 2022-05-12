860dfa36aa4ea96ee6f4091013ef1e89a8a0c5ee
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-13 00:10:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ bot.start()
 @anibot.on(events.NewMessage(pattern="/start"))
 async def _(event):
     ok = await anibot(GetFullUserRequest(event.sender_id))
-    await event.reply(f"Hi ||{ok.user.first_name}||!\n\nI am an Auto Anime bot!!\n\nRead /help to know more about me\n\nA part of @ArrayCore\n\n Made With ❤ By @TheVenomXD")
+    await event.reply(f"Hi {ok.user.first_name}!\n\nPlease use /help to get started.\n\nNote: I can only give you the files of anime that are currently bring airied at @AutoAnimeUploads.")
 
 @bot.on(events.NewMessage(incoming=True, chats=Var.FROM_CHANNEL))
 async def _(event): 
