1cbe79e322be40bbc0bcd39218ded06bff5e4ade
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-13 15:35:23 +0530

    Update Lion.py

--- Lion.py
+++ Lion.py
@@ -6,7 +6,7 @@ logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 # Starting bot
-system("git clone https://github.com/desinobita/AutoAnimeBot/tree/main && cd AutoAnimeBot && python3 -m bot.py")
+system("git clone https://github.com/desinobita/AutoAnimeBot && cd AutoAnimeBot && python3 -m bot.py")
 # if got error
 print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ")
 # Script written for stop heroku suspension
