5790a81dc50478cec2b79aedf1b8860ee1e990fd
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-13 15:25:33 +0530

    Add files via upload

---
+++ Lion.py
@@ -0,0 +1,15 @@
+import logging
+from os import system
+
+# Logs
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+)
+# Starting bot
+system("git clone https://github.com/desinobita/AutoAnimeBot/tree/main && cd AutoAnimeBot && python3 -m bot.py")
+# if got error
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ")
+# Script written for stop heroku suspension
+
+
+#Don't Try To Steal Without Credit @Simpleboy786
