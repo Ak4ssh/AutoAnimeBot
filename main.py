8cb9eb242ae84eee6fbef959d8348085827cc2db
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-11-21 17:51:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ async def _(e):
             ]
         ]
         await e.client.edit_message(e.chat.id, e.query.msg_id, f"**Welcome Sir!\n\nI'm Predictor Aviator Bot \nMade for Predicting Signal From Different Sites\n\nMade with ❤️ By @TheVenomXD**", buttons=but)
-            elif e.data == b"Home":
+    elif e.data == b"Home":
     but = [
         [
             telethon.Button.inline("Sites", b"Acc"),
